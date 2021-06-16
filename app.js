1b807b0e3aaf3f3532ecf2d6d35fff48ea1db143
Deivid <davidvalenciamontes7@gmail.com>
2021-06-16 13:45:38 -0500

    Crear rutas a Lugares

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-const Place = require('./models/Place')
+
+const  places = require('./routes/places')
 
 const DB = require('./config/database')
+
+
 DB.connect();
-
-
 var app = express();
 
 
@@ -18,78 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.post('/places', (req, res) => {
-  Place.create({
-    title: req.body.title,
-    description : req.body.description,
-    acceptsCreditCard : req.body.acceptsCreditCard,
-    openHour : req.body.openHour,
-    closeHour: req.body.closeHour
-  })
-    .then( doc => {
-      res.json(doc)
-    })
-    .catch(err => {
-      console.log(err);
-      res.json(err)
-    })
-})
-
-app.get('/places', (req,res) => {
-  Place.find({})
-    .then( docs => {
-      res.json(docs)
-    })
-    .catch(err => {
-      res.json(err)
-    })
-})
-
-app.get('/places/:id', (req, res) => {
-  Place.findById(req.params.id)
-    .then(doc => {
-      res.json(doc)
-    })
-    .catch(err => {
-      res.json(err)
-    })
-})
-
-app.put('/places/:id', (req,res) => {
-/*
-let attributes = ['title','description','atcceptsCreditCard','openHour','closeHour']
-let placeParams = {}
-attributes.forEach(attr => {
-  if(Object.prototype.hasOwnProperty.call(req.body,attr))
-    placeParams[attr] = req.body[attr]
-})
-*/
-
-  let placeParams = {title,description,acceptsCreditCard,openHour,closeHour} = req.body;
-  
-  Place.findByIdAndUpdate(
-    req.params.id,
-    placeParams
-    ,{
-      new:true
-    })
-    .then(doc => {
-      res.json(doc)
-    })
-    .catch(err => {
-      res.json(err)
-    })
-})
-
-app.delete('/places/:id', (req,res) => {
-  Place.findByIdAndRemove(req.params.id)
-    .then(doc => {
-      res.json({})
-    })
-    .catch(err => {
-      res.json(err)
-    })
-})
+app.use('/places',places) //montar el router de places
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
